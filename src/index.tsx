2f07c55142fff08ff44fa4611835238c04fdb449
ArinaKumeisha <arinanozdracheva@mail.ru>
2022-05-23 22:00:07 +0300

    change Browser Router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { App } from 'App';
 import { store } from 'redux/store';
@@ -11,9 +11,9 @@ import { store } from 'redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
     ,
   </Provider>,
 );
